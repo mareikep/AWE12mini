9b60457c500e92420638d69730fc8d286d7924ac
Georg Modzelewski <melitta@tzi.de>
2012-03-05 14:20:57 +0100

    annotate drin. Ausführen und aktualisieren mit bundle exec annotate

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,14 @@
 class Group < ActiveRecord::Base
 	has_many :users
 end
+# == Schema Information
+#
+# Table name: groups
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  task       :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
 	belongs_to :user
 end
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
