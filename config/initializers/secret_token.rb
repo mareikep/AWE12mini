9f5692f8ce7a819189027315b3e6bded9addbf38
mareike <mareikep@tzi.de>
2012-03-05 10:47:38 +0100

    Git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miniproject::Application.config.secret_token = 'e9e8ae8a69ed5ad64ae9d1969c41850a568f734da945d8400dddb886e750cf971a325de3c151c084552110978c3c3fedd851cba102537b62c9b564cb7fa9ce90'
